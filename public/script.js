af87a1057af5c017a02df1eaa804cf721656eb64
on-stu <minsu0523@naver.com>
2021-11-04 18:15:27 +0900

    zoom clone done

---
+++ public/script.js
@@ -0,0 +1,132 @@
+const socket = io("/");
+
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+const textInput = document.getElementById("chat_message");
+const ul = document.querySelector("ul");
+
+myVideo.muted = true;
+
+let myVideoStream;
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, myVideoStream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+
+    textInput.addEventListener("keydown", (e) => {
+      if (e.key === "Enter" && textInput.value !== "") {
+        socket.emit("message", textInput.value);
+        textInput.value = "";
+      }
+    });
+
+    socket.on("createMessage", (message) => {
+      const li = document.createElement("li");
+      li.setAttribute("class", "message");
+      li.innerHTML = `<b>손 다인</b><br/>${message}`;
+      ul.append(li);
+      scrollToBottom();
+    });
+  });
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
+
+const scrollToBottom = () => {
+  var mainChatWindow = document.getElementsByClassName("main__chat_window");
+  mainChatWindow[0].scrollTop = mainChatWindow[0].scrollHeight;
+};
+
+const muteUnmute = () => {
+  let enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    setUnmuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = false;
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>음소거</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>음소거 해제</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>비디오 중지</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>비디오 시작</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
